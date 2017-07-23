97ecee428f159780d8659805ca3ae09cdd37c1e6
Angelo Moscati <moscati@gmail.com>
2017-07-23 17:31:34 +0300

    First draft

---
+++ Restarter/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Restarter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.checkedListBox1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(225, 307);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.AllowDrop = true;
+            this.checkedListBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(280, 259);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(225, 307);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ Restarter/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management;
+
+namespace Restarter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public struct ProcessInfo
+        {
+            public string Name { get; set; }
+            public string CmdLine { get; set; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            // the code that you want to measure comes here
+            
+            List<object> processes = new List<object>();
+            var localProcesses = Process.GetProcesses().Where(
+                x => x.SessionId == Process.GetCurrentProcess().SessionId &&
+                x.MainWindowTitle.Length > 0).ToDictionary(x => x.Id);
+
+            ManagementClass mgmtClass = new ManagementClass("Win32_Process");
+            foreach (ManagementObject process in mgmtClass.GetInstances())
+            {
+                // Only current session (user)
+                if (process["SessionId"].ToString() != Process.GetCurrentProcess().SessionId.ToString())
+                {
+                    continue;
+                }
+
+                // Only UI processes
+                uint id = (uint)process["ProcessId"];
+                if (!localProcesses.ContainsKey((int)id))
+                {
+                    continue;
+                }
+
+                string processName = process["Name"].ToString();
+
+                object cmdLine = process["CommandLine"];
+                if (cmdLine != null)
+                {
+                    ProcessInfo pi = new ProcessInfo ();
+                    pi.Name = processName;
+                    pi.CmdLine = cmdLine.ToString();
+                    processes.Add(pi);
+                }
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            //checkedListBox1.Items.AddRange(processes.ToArray());
+            //checkedListBox1.DisplayMember = "CmdLine";
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            checkedListBox1.Size = checkedListBox1.Parent.Size;
+        }
+    }
+
+    public static class Utilities
+    {
+        public static string GetCommandLine(this Process process)
+        {
+            var commandLine = new StringBuilder(process.MainModule.FileName);
+
+            commandLine.Append(" ");
+            using (var searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + process.Id))
+            {
+                foreach (var @object in searcher.Get())
+                {
+                    commandLine.Append(@object["CommandLine"]);
+                    commandLine.Append(" ");
+                }
+            }
+
+            return commandLine.ToString();
+        }
+    }
+}
+
