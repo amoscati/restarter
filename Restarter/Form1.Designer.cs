19a5c330904b540c75268f7067456c8505cf341b
Angelo Moscati <moscati@gmail.com>
2017-07-26 16:02:51 +0300

    Remove cmd dependency

--- Restarter/Form1.Designer.cs
+++ Restarter/Form1.Designer.cs
@@ -29,39 +29,151 @@
         private void InitializeComponent()
         {
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabelRefresh = new System.Windows.Forms.LinkLabel();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel3);
+            this.flowLayoutPanel1.Controls.Add(this.treeView1);
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel4);
             this.flowLayoutPanel1.Controls.Add(this.checkedListBox1);
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel2);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(225, 307);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(236, 661);
             this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(3, 22);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(222, 304);
+            this.treeView1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoSize = true;
+            this.flowLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel2.Controls.Add(this.button1);
+            this.flowLayoutPanel2.Controls.Add(this.button2);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 453);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(222, 29);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(84, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Launch";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.AutoSize = true;
+            this.flowLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel3.Controls.Add(this.label1);
+            this.flowLayoutPanel3.Controls.Add(this.linkLabelRefresh);
+            this.flowLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(222, 13);
+            this.flowLayoutPanel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Running processes";
+            // 
+            // linkLabelRefresh
+            // 
+            this.linkLabelRefresh.AutoSize = true;
+            this.linkLabelRefresh.Location = new System.Drawing.Point(107, 0);
+            this.linkLabelRefresh.Name = "linkLabelRefresh";
+            this.linkLabelRefresh.Size = new System.Drawing.Size(44, 13);
+            this.linkLabelRefresh.TabIndex = 4;
+            this.linkLabelRefresh.TabStop = true;
+            this.linkLabelRefresh.Text = "Refresh";
+            this.linkLabelRefresh.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.AutoSize = true;
+            this.flowLayoutPanel4.Controls.Add(this.label2);
+            this.flowLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(3, 332);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(222, 13);
+            this.flowLayoutPanel4.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Processes to run";
             // 
             // checkedListBox1
             // 
-            this.checkedListBox1.AllowDrop = true;
-            this.checkedListBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkedListBox1.Location = new System.Drawing.Point(3, 351);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(280, 259);
-            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.Size = new System.Drawing.Size(222, 96);
+            this.checkedListBox1.TabIndex = 4;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(225, 307);
+            this.ClientSize = new System.Drawing.Size(236, 661);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Restarter";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.flowLayoutPanel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +181,15 @@
         #endregion
 
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabelRefresh;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
     }
 }
--- Restarter/Form1.cs
+++ Restarter/Form1.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Management;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace Restarter
 {
@@ -27,14 +30,110 @@ namespace Restarter
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            // the code that you want to measure comes here
+            loadProcessList();
+
+            refreshRunningProcesses();      
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //checkedListBox1.Size = checkedListBox1.Parent.Size;
+            //flowLayoutPanel1.Size = new Size(this.Size.Width, this.Size.Height - flowLayoutPanel2.Size.Height);
+            treeView1.Size = new Size(this.Size.Width, treeView1.Size.Height);
             
-            List<object> processes = new List<object>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Save
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("restarter.bin", FileMode.Create,
+                FileAccess.Write, FileShare.None);
+            List<string> processes = new List<string>();
+
+            foreach (TreeNode node in treeView1.Nodes)
+            {
+                foreach (TreeNode cmdLineNode in node.Nodes)
+                {
+                    if (cmdLineNode.Checked) 
+                    {
+                        processes.Add(cmdLineNode.Text);
+                    }
+                }
+                
+            }
+            formatter.Serialize(stream, processes);
+            stream.Close();
+
+            loadProcessList();
+        }
+
+        private void loadProcessList()
+        {
+            // Load
+            checkedListBox1.Items.Clear();
+
+            try
+            {
+                IFormatter formatter = new BinaryFormatter();
+                Stream stream = new FileStream("restarter.bin", FileMode.Open,
+                    FileAccess.Read, FileShare.None);
+                List<string> processes = (List<string>)formatter.Deserialize(stream);
+                stream.Close();
+
+                foreach (var cmdLine in processes)
+                {
+                    checkedListBox1.Items.Add(cmdLine, true);
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var cmd in checkedListBox1.CheckedItems)
+            {
+                string cmdLine = cmd.ToString();
+
+                string executable;
+                string args;
+                if (cmdLine[0] == '"')
+                {
+                    int i = cmdLine.IndexOf('"', 1);
+                    executable = cmdLine.Substring(0, i + 1);
+                    args = cmdLine.Substring(i + 1);
+
+                }
+                else
+                {
+                    int i = cmdLine.IndexOf(' ', 1);
+                    executable = cmdLine.Substring(0, i + 1);
+                    args = cmdLine.Substring(i);
+                }
+
+                Process process = new Process();
+                // Configure the process using the StartInfo properties.
+                process.StartInfo.FileName = executable.Trim();
+                process.StartInfo.Arguments = args.Trim();
+                process.StartInfo.UseShellExecute = true;
+                process.Start();
+            }
+           
+        }
+
+        private void refreshRunningProcesses()
+        {
+            // Refresh
+            treeView1.Nodes.Clear();
+
             var localProcesses = Process.GetProcesses().Where(
                 x => x.SessionId == Process.GetCurrentProcess().SessionId &&
                 x.MainWindowTitle.Length > 0).ToDictionary(x => x.Id);
 
+            int maxChars = 0;
             ManagementClass mgmtClass = new ManagementClass("Win32_Process");
             foreach (ManagementObject process in mgmtClass.GetInstances())
             {
@@ -56,24 +155,39 @@ namespace Restarter
                 object cmdLine = process["CommandLine"];
                 if (cmdLine != null)
                 {
-                    ProcessInfo pi = new ProcessInfo ();
+                    ProcessInfo pi = new ProcessInfo();
                     pi.Name = processName;
                     pi.CmdLine = cmdLine.ToString();
-                    processes.Add(pi);
+
+                    var nameNode = treeView1.Nodes[processName];
+                    if (nameNode == null)
+                    {
+                        nameNode = new TreeNode(processName);
+                        nameNode.Name = processName;
+                        treeView1.Nodes.Add(nameNode);
+                    }
+
+                    var cmdLineLength = cmdLine.ToString().Length;
+                    maxChars = maxChars < cmdLineLength ? cmdLineLength : maxChars;
+                    var cmdNode = new TreeNode(cmdLine.ToString());
+                    if (checkedListBox1.Items.Contains(cmdLine)) 
+                    {
+                        cmdNode.Checked = true;
+                    }
+                    nameNode.Nodes.Add(cmdNode);
                 }
             }
 
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
+            Size = new Size(maxChars * 6, Size.Height); // Empiric size
+            treeView1.ExpandAll();
 
-            //checkedListBox1.Items.AddRange(processes.ToArray());
-            //checkedListBox1.DisplayMember = "CmdLine";
+            Form1_Resize(this, null);
         }
 
-        private void Form1_Resize(object sender, EventArgs e)
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            checkedListBox1.Size = checkedListBox1.Parent.Size;
-        }
+            refreshRunningProcesses();
+        }        
     }
 
     public static class Utilities
